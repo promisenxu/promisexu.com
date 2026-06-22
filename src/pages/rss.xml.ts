a2bf7dc80ec619ac5ac86b33c134dcdcf5de7589
Promise Xu <passybypromise@hotmail.com>
2026-06-22 10:25:58 -0400

    feat: add publication launch pages

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+import rss from "@astrojs/rss";
+import { getPublishedWritings, getWritingUrl } from "@/lib/content";
+import { site } from "@/lib/site";
+
+export async function GET() {
+  const writings = await getPublishedWritings();
+
+  return rss({
+    title: site.title,
+    description: site.description,
+    site: site.url,
+    items: writings.map((entry) => ({
+      title: entry.data.title,
+      description: entry.data.description,
+      pubDate: entry.data.date,
+      link: getWritingUrl(entry)
+    }))
+  });
+}
